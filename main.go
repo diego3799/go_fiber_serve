ee9512436d4c557cf72303c97b185efd7de9833e
Diego Zarate <diego3799@gmail.com>
2023-01-17 21:58:52 -0600

    added air

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
 
-func main(){
-	fmt.Println("Hello, World!")
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	err := godotenv.Load()
+	if err == nil {
+		log.Fatal("Error loading .env file")
+	}
+	PORT := os.Getenv("PORT")
+	app := fiber.New()
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+	app.Listen(PORT)
 }
